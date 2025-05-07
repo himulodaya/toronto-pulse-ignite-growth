5df57dff2313edeef48e8dcac9dfe0fdd5e80753
himulodaya <47130025+himulodaya@users.noreply.github.com>
2025-05-07 13:00:02 -0400

    Added MS Clarity

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import clarity from '@microsoft/clarity';
 
+const CLARITY_PROJECT_ID = "rfxft0c156";
+clarity.initialize(rfxft0c156);
 createRoot(document.getElementById("root")!).render(<App />);
